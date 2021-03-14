7b0f75341f51be5c4d6eadaf129282c6c587b7f7
Savannah Moe <savannah_moe@baylor.edu>
2021-03-14 17:39:39 -0500

    adding high score to game screen and keep high score unless updated new score at the end

--- alien_invasion.py
+++ alien_invasion.py
@@ -159,6 +159,7 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)  #
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:  # check is alien group is empty
             # destroy existing bullets and create a new fleet
--- game_stats.py
+++ game_stats.py
@@ -7,6 +7,8 @@ class GameStats:
         self.reset_stats()
         # start game in an inactive state
         self.game_active = False
+        # highscore should never be reset.
+        self.high_score = 0
 
     def reset_stats(self):
         """Initilize statistics that can change during the game."""
